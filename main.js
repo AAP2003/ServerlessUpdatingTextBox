3365ae34ac9e51593c59443afdc3228dd0725c2c
Aarya Patel <pate1464@purdue.edu>
2021-12-15 00:01:40 -0500

    Using gun servers, will swtich to AXE

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const gun = GUN();
+const gun = GUN(['https://gun-manhattan.herokuapp.com/gun']);
 
 paste = document.getElementById("paste");
 
 copy = gun.get('test').get('paste');
 paste.oninput = () => { copy.put(paste.value) };
-copy.on((data) => { paste.value = data });
+copy.on((data) => { paste.value = data }, true);
