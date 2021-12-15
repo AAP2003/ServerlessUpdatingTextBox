9dcc5f20165f618b32b61c2858efc38a42e2ca84
Aarya Patel <pate1464@purdue.edu>
2021-12-15 00:03:12 -0500

    Testing different database keys

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const gun = GUN(['https://gun-manhattan.herokuapp.com/gun']);
 
 paste = document.getElementById("paste");
 
-copy = gun.get('test').get('paste');
+copy = gun.get('test').get('paste_');
 paste.oninput = () => { copy.put(paste.value) };
 copy.on((data) => { paste.value = data }, true);
